10875243f1b33e27072320f5eb015d09ae35590c
МС-77астра <rusmen36@mail.ru>
2021-11-06 18:52:37 +0300

    урок 2

--- src/main/java/FirstProgram.java
+++ src/main/java/FirstProgram.java
@@ -1,5 +1,27 @@
 public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Привет Мир!");
+
+        long summa = summa(9, 6);
+        System.out.println(summa);
+        long minsumma = minus(9, 6);
+        System.out.println(minsumma);
+        long minsumm = multiply(9, 6);
+        System.out.println(minsumm);
+    }
+
+    public static int summa(int arg1, int arg2) {
+        int result = arg1 + arg2;
+        return result;
+    }
+
+    public static int minus(int arg1, int arg2) {
+        int result = arg1 - arg2;
+        return result;
+    }
+
+    public static int multiply(int arg1, int arg2) {
+        int result = arg1 * arg2;
+        return result;
     }
 }
