0b39d84ecb172cf180f8a504a2ecb74c2819f56e
МС-77астра <rusmen36@mail.ru>
2021-11-10 19:39:27 +0400

    решение квадратного уравнения

--- src/main/java/QvadratishPractic.java
+++ src/main/java/QvadratishPractic.java
@@ -2,17 +2,17 @@ public class QvadratishPractic {
     public static void main(String[] args) {
         double a, b, c;
         double x1, x2;
-        a = 9;
-        b = 3;
-        c = 4;
-        double Discrimenant = b * b - 4 * a * c;
-        if (Discrimenant == 0) {
+        a = 1;
+        b = 5;
+        c = 6;
+        double discrimenant = b * b - 4 * a * c;
+        if (discrimenant == 0) {
             x1 = (-b) / (2 * a);
             System.out.printf("x1 = x2 = %s", x1);
-        } else if (Discrimenant > 0) {
-            x1 = (-b + Math.sqrt(Discrimenant)) / (2 * a);
-            x2 = (-b - Math.sqrt(Discrimenant)) / (2 * a);
-            System.out.printf("x1 = %s,x2 = %s,x1,x2");
+        } else if (discrimenant > 0) {
+            x1 = (-b + Math.sqrt(discrimenant)) / (2 * a);
+            x2 = (-b - Math.sqrt(discrimenant)) / (2 * a);
+            System.out.printf("x1 = %s,x2 = %s",x1,x2);
         } else {
             System.out.println("нет решения так, как дискреминант < 0");
         }
